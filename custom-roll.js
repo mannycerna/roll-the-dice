332a6ad5643dd7d5b27c0bcf9ac4089dffbab398
mannycerna <manuel.cerna@codeimmersives.com>
2022-09-22 15:53:22 -0500

    Corrected per Tyler Silva feedback:  Casting the prompt varible and asking for a number from user

--- custom-roll.js
+++ custom-roll.js
@@ -2,7 +2,7 @@
 
 const prompt = require('prompt-sync')({sigint: true});
 
-let numSides = prompt("Welcome User. How many sided die do you wish to use: ");
+let numSides = Number(prompt("Welcome. Please enter a number which identifies the number of sides in dice: "));
 
  let result = Math.ceil(Math.random() * numSides);
  console.log(result);
